6b658dee465ee8a77e8ef0a09eebd7fb67b9c300
Leonardo Bevilacqua <leonardo.bevilacqua@eldorado.org.br>
2021-08-12 11:57:11 -0300

    adding sections effect

--- scripts/page-effects.js
+++ scripts/page-effects.js
@@ -1,5 +1,11 @@
 /* Profile panel effect */
 let profile_panel_is_opened = false;
+
+const section_is_opened = {
+    'skills': true,
+    'experiences': false,
+    'portfolio-cards': false
+}
 const profile_infos_elem = document.getElementById('profile-infos');
 const toggle_profile_panel = (event) => {
     profile_panel_is_opened = !profile_panel_is_opened;
@@ -11,5 +17,22 @@ const toggle_profile_panel = (event) => {
         event.children[0].classList.add('d-none');
         event.children[1].classList.remove('d-none');
         profile_infos_elem.classList.remove('hidden');
+    }
+}
+
+/* Sections effect */
+const toggle_section = (event, section_id) => {
+    section_is_opened[section_id] = !section_is_opened[section_id];
+    console.log(section_is_opened[section_id])
+    
+    const section = document.getElementById(section_id);
+    if (!section_is_opened[section_id]) {
+        event.children[0].classList.remove('d-none');
+        event.children[1].classList.add('d-none');
+        section.classList.add('hidden');
+    } else {
+        event.children[0].classList.add('d-none');
+        event.children[1].classList.remove('d-none');
+        section.classList.remove('hidden');
     }
 }
