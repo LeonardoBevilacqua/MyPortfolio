e24d835e67fed9fb23686da35291008f2d93d2ad
Leonardo Bevilacqua <leonardo.bevilacqua@eldorado.org.br>
2021-08-12 11:00:10 -0300

    finising profile panel animation

--- scripts/page-effects.js
+++ scripts/page-effects.js
@@ -1,4 +1,15 @@
-function toggle_profile_panel(event) {
-    event.append('tet')
-    console.log(event)
+/* Profile panel effect */
+let profile_panel_is_opened = true;
+const profile_infos_elem = document.getElementById('profile-infos');
+const toggle_profile_panel = (event) => {
+    profile_panel_is_opened = !profile_panel_is_opened;
+    if (!profile_panel_is_opened) {
+        event.children['collapse-button-open'].classList.remove('d-none');
+        event.children['collapse-button-close'].classList.add('d-none');
+        profile_infos_elem.classList.add('hidden');
+    } else {
+        event.children['collapse-button-open'].classList.add('d-none');
+        event.children['collapse-button-close'].classList.remove('d-none');
+        profile_infos_elem.classList.remove('hidden');
+    }
 }
