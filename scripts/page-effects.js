450fd2d2882c87bb4cd6c123441d1ce32572fad8
Leonardo Bevilacqua <leonardo.bevilacqua@eldorado.org.br>
2021-08-12 16:39:36 -0300

    adding label to profile panel button

--- scripts/page-effects.js
+++ scripts/page-effects.js
@@ -11,11 +11,13 @@ const toggle_profile_panel = (event) => {
     profile_panel_is_opened = !profile_panel_is_opened;
     if (!profile_panel_is_opened) {
         event.children[0].classList.remove('d-none');
-        event.children[1].classList.add('d-none');
+        event.children[1].classList.remove('d-none');
+        event.children[2].classList.add('d-none');
         profile_infos_elem.classList.add('hidden');
     } else {
         event.children[0].classList.add('d-none');
-        event.children[1].classList.remove('d-none');
+        event.children[1].classList.add('d-none');
+        event.children[2].classList.remove('d-none');
         profile_infos_elem.classList.remove('hidden');
     }
 }
