52492033046d8497faf34f4ac17e16c98206c468
Leonardo Bevilacqua <leonardo.bevilacqua@eldorado.org.br>
2021-08-12 11:28:53 -0300

    adding extra effect to profile panel

--- scripts/page-effects.js
+++ scripts/page-effects.js
@@ -1,15 +1,15 @@
 /* Profile panel effect */
-let profile_panel_is_opened = true;
+let profile_panel_is_opened = false;
 const profile_infos_elem = document.getElementById('profile-infos');
 const toggle_profile_panel = (event) => {
     profile_panel_is_opened = !profile_panel_is_opened;
     if (!profile_panel_is_opened) {
-        event.children['collapse-button-open'].classList.remove('d-none');
-        event.children['collapse-button-close'].classList.add('d-none');
+        event.children[0].classList.remove('d-none');
+        event.children[1].classList.add('d-none');
         profile_infos_elem.classList.add('hidden');
     } else {
-        event.children['collapse-button-open'].classList.add('d-none');
-        event.children['collapse-button-close'].classList.remove('d-none');
+        event.children[0].classList.add('d-none');
+        event.children[1].classList.remove('d-none');
         profile_infos_elem.classList.remove('hidden');
     }
 }
