9ab0ce8ecbba2acaa5af0828316643d0022c5a63
Leonardo <leonardo_bevilacqua@hotmail.com>
2022-07-03 13:50:43 -0300

    creating experience section

---
+++ src/lib/experience/ExperienceCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let experience: {
+		title: string;
+		subtitle: string;
+		start: number;
+		end: number | null;
+		descriptions: string[];
+	};
+</script>
+
+<div class="bg-slate-300 dark:bg-slate-900 text-black dark:text-white p-3 shadow-lg">
+    <h3 class="font-bold text-lg">{experience.title}</h3>
+    <h4 class="font-semibold text-sm">{experience.subtitle}, {experience.start} - {experience.end ? experience.end : 'Atualmente'}</h4>
+    {#each experience.descriptions as description, index (index)}
+        <p class="my-3">{description}</p>
+    {/each}
+</div>
