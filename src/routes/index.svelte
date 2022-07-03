695cfed57689b592bf4e15b0cb2de5f83eaf191c
Leonardo <leonardo_bevilacqua@hotmail.com>
2022-07-03 11:29:35 -0300

    adding gap

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,7 @@
 
 <main class="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-6 h-screen font-Oswald">
 	<ProfilePanel />
-	<div class="grid grid-rows-3 col-span-2 2xl:col-span-5 bg-slate-100 dark:bg-slate-800 p-3">
+	<div class="grid grid-rows-3 gap-3 col-span-2 2xl:col-span-5 bg-slate-100 dark:bg-slate-800 p-3">
 		<SkillSection />
 		<ExperienceSection />
 		<PortfolioSection />
