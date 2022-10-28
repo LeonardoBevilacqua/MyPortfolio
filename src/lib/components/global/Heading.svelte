3ba068e7784e6e96ae98178ff5218afd55db2a5b
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2022-10-28 15:03:10 -0300

    starting curriculum page

---
+++ src/lib/components/global/Heading.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let className = '';
+	export let h2 = false;
+	export let h3 = false;
+</script>
+
+{#if h2}
+	<h2 class="{className} text-2xl"><slot /></h2>
+{:else if h3}
+	<h3 class="{className} font-bold text-xl"><slot /></h3>
+{:else}
+	<h1 class="{className} font-bold text-5xl"><slot /></h1>
+{/if}
---
+++ src/routes/curriculum/index@curriculum.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import Heading from '$lib/components/global/Heading.svelte';
+	import { ExperienceStore } from '$lib/store/Experience.store';
+	import { SkillStore } from '$lib/store/Skill.store';
+</script>
+
+<div class="col-span-2 p-3 grid gap-3">
+	<section>
+		<Heading>Leonardo Almeida Bevilacqua</Heading>
+		<Heading h2>Analista de Software Pleno na Instituto de Pesquisas Eldorado</Heading>
+		<Heading h2 className="text-neutral-500">Campinas, São Paulo, Brasil</Heading>
+	</section>
+
+	<section>
+		<Heading h3>Perfil Profissional</Heading>
+		<p>
+			Formado em técnico em informática, na Escola SENAI Prof. Dr. Euryclides de Jesus Zerbini e
+			Ciências da computação, na faculdade DeVry Metrocamp.
+		</p>
+		<p>
+			Trabalhei como estagiário e funcionário na Empresa VISIO TECNOLOGIA, trabalhando com a
+			linguagem de programação 4GL e os bancos de dados Oracle, Informix e SQLServer.
+		</p>
+		<p>
+			Atuei como estagiário, no Instituto Eldorado Campinas, e atualmente como analista de software.
+		</p>
+		<p>Pós graduado em engenharia de software.</p>
+	</section>
+
+	<section>
+		<Heading h3>Objetivos</Heading>
+		<p>TODO</p>
+	</section>
+
+	<section>
+		<Heading h3>Experiência</Heading>
+		<ul>
+			{#each $ExperienceStore as experience, index (index)}
+				<li>{experience.title}</li>
+			{/each}
+		</ul>
+	</section>
+
+	<section>
+		<Heading h3>Formação acadêmica</Heading>
+		<ul>
+			{#each $ExperienceStore as experience, index (index)}
+				<li>{experience.title}</li>
+			{/each}
+		</ul>
+	</section>
+</div>
+
+<div class="order-first bg-sky-900 text-white p-3">
+	<section>
+		<Heading h3>Contato</Heading>
+		<ul>
+			<li>(19)99582-8664 (Mobile)</li>
+			<li>leonardo_bevilacqua@hotmail.com</li>
+			<li>www.linkedin.com/in/leonardoabevilacqua (LinkedIn)</li>
+			<li>leonardobevilacqua.com.br/ (Portfolio)</li>
+		</ul>
+	</section>
+
+	<section>
+		<Heading h3>Principais competências</Heading>
+		<ul>
+			{#each $SkillStore as skill, index (index)}
+				<li>{skill.title}</li>
+			{/each}
+		</ul>
+	</section>
+
+	<section>
+		<Heading h3>Languages</Heading>
+		<ul>
+			<li>Português (Native or Bilingual)</li>
+			<li>Inglês (Full Professional)</li>
+		</ul>
+	</section>
+</div>
