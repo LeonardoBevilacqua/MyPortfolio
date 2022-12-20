af3b28d1ab9d4443a05a272347d6eb8d46370432
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2022-12-20 14:02:17 -0300

    removing pdfmake

--- src/lib/components/global/Nav.svelte
+++ src/lib/components/global/Nav.svelte
@@ -1,21 +1,20 @@
 <script>
 	import { page } from '$app/stores';
-    import { generateCurriculum } from "$lib/curriculum";
 </script>
 
 <div class="flex">
 	<nav
 		class="p-5 bg-light-30 dark:bg-dark-30 text-dark-default dark:text-dark-default inline-flex gap-5 m-auto justify-center rounded-3xl text-xl md:text-2xl"
 	>
-		<a sveltekit:noscroll sveltekit:prefetch class:hidden={$page.url.pathname === '/'} href="/#main">Competências</a>
+		<a data-sveltekit-preload-data class:hidden={$page.url.pathname === '/'} href="/#main">Competências</a>
 		<a
-			sveltekit:noscroll sveltekit:prefetch
+			data-sveltekit-preload-data
 			class:hidden={$page.url.pathname === '/experience'}
 			href="/experience#main">Experiências</a
 		>
-		<a sveltekit:noscroll sveltekit:prefetch class:hidden={$page.url.pathname === '/portfolio'} href="/portfolio#main"
+		<a data-sveltekit-preload-data class:hidden={$page.url.pathname === '/portfolio'} href="/portfolio#main"
 			>Portfólio</a
 		>
-        <button on:click={generateCurriculum}>Currículo</button>
+        <!-- <button on:click={generateCurriculum}>Currículo</button> -->
 	</nav>
 </div>
