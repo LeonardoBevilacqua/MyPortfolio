99efb5b4424362e94315dd196e9f1da273485541
Leonardo <leonardo_bevilacqua@hotmail.com>
2023-04-29 11:19:06 -0300

    wrapping nav

--- src/lib/components/global/Nav.svelte
+++ src/lib/components/global/Nav.svelte
@@ -12,7 +12,7 @@
 
 <div class="flex">
 	<nav
-		class="p-5 bg-light-30 dark:bg-dark-30 text-dark-default dark:text-dark-default inline-flex gap-5 m-auto justify-center rounded-3xl text-xl md:text-2xl"
+		class="p-5 bg-light-30 dark:bg-dark-30 text-dark-default dark:text-dark-default inline-flex flex-wrap gap-5 m-auto justify-center rounded-3xl text-xl md:text-2xl"
 	>
 		<a data-sveltekit-preload-data class:hidden={$page.url.pathname === '/'} href="/#main"
 			>{$t('interface.skillTitle')}</a
