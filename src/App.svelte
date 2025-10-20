b306b35f4d0e5b0bd06390798b09e715d4272213
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2025-10-20 13:18:06 -0300

    create base layout for the portfolio

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,165 @@
-<main>
-    <h1 class="text-3xl font-bold underline">Hello world</h1>
+<div id="left-panel" class="panel">left panel</div>
+<nav id="right-panel-header" class="panel">right panel header</nav>
+<main id="right-panel-body" class="panel">
+    <h1>right panel body</h1>
+    <section>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at risus
+            vitae lorem vehicula pretium. Vivamus quis porttitor ex. Vivamus et
+            turpis venenatis, pellentesque libero eget, tristique nibh. Aenean
+            porta ipsum leo, nec eleifend metus semper vitae. Duis dapibus orci
+            purus, eget tincidunt metus laoreet sed. Pellentesque at leo
+            venenatis, accumsan tortor vitae, pretium purus. Suspendisse
+            convallis urna non nunc blandit bibendum. Vivamus commodo gravida
+            sodales. Proin dictum nulla non diam faucibus tincidunt. Nam
+            pharetra ex non ante pharetra, quis gravida justo tempus. Sed at
+            augue eu enim feugiat vehicula. Fusce nec eros finibus, suscipit
+            ante ac, mollis nunc. Duis condimentum, neque sit amet fringilla
+            suscipit, velit mauris semper erat, eget elementum lacus massa et
+            erat. Etiam luctus malesuada sollicitudin.
+        </p>
+    </section>
+    <section>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at risus
+            vitae lorem vehicula pretium. Vivamus quis porttitor ex. Vivamus et
+            turpis venenatis, pellentesque libero eget, tristique nibh. Aenean
+            porta ipsum leo, nec eleifend metus semper vitae. Duis dapibus orci
+            purus, eget tincidunt metus laoreet sed. Pellentesque at leo
+            venenatis, accumsan tortor vitae, pretium purus. Suspendisse
+            convallis urna non nunc blandit bibendum. Vivamus commodo gravida
+            sodales. Proin dictum nulla non diam faucibus tincidunt. Nam
+            pharetra ex non ante pharetra, quis gravida justo tempus. Sed at
+            augue eu enim feugiat vehicula. Fusce nec eros finibus, suscipit
+            ante ac, mollis nunc. Duis condimentum, neque sit amet fringilla
+            suscipit, velit mauris semper erat, eget elementum lacus massa et
+            erat. Etiam luctus malesuada sollicitudin.
+        </p>
+    </section>
+    <section>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at risus
+            vitae lorem vehicula pretium. Vivamus quis porttitor ex. Vivamus et
+            turpis venenatis, pellentesque libero eget, tristique nibh. Aenean
+            porta ipsum leo, nec eleifend metus semper vitae. Duis dapibus orci
+            purus, eget tincidunt metus laoreet sed. Pellentesque at leo
+            venenatis, accumsan tortor vitae, pretium purus. Suspendisse
+            convallis urna non nunc blandit bibendum. Vivamus commodo gravida
+            sodales. Proin dictum nulla non diam faucibus tincidunt. Nam
+            pharetra ex non ante pharetra, quis gravida justo tempus. Sed at
+            augue eu enim feugiat vehicula. Fusce nec eros finibus, suscipit
+            ante ac, mollis nunc. Duis condimentum, neque sit amet fringilla
+            suscipit, velit mauris semper erat, eget elementum lacus massa et
+            erat. Etiam luctus malesuada sollicitudin.
+        </p>
+    </section>
+    <section>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at risus
+            vitae lorem vehicula pretium. Vivamus quis porttitor ex. Vivamus et
+            turpis venenatis, pellentesque libero eget, tristique nibh. Aenean
+            porta ipsum leo, nec eleifend metus semper vitae. Duis dapibus orci
+            purus, eget tincidunt metus laoreet sed. Pellentesque at leo
+            venenatis, accumsan tortor vitae, pretium purus. Suspendisse
+            convallis urna non nunc blandit bibendum. Vivamus commodo gravida
+            sodales. Proin dictum nulla non diam faucibus tincidunt. Nam
+            pharetra ex non ante pharetra, quis gravida justo tempus. Sed at
+            augue eu enim feugiat vehicula. Fusce nec eros finibus, suscipit
+            ante ac, mollis nunc. Duis condimentum, neque sit amet fringilla
+            suscipit, velit mauris semper erat, eget elementum lacus massa et
+            erat. Etiam luctus malesuada sollicitudin.
+        </p>
+    </section>
+    <section>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at risus
+            vitae lorem vehicula pretium. Vivamus quis porttitor ex. Vivamus et
+            turpis venenatis, pellentesque libero eget, tristique nibh. Aenean
+            porta ipsum leo, nec eleifend metus semper vitae. Duis dapibus orci
+            purus, eget tincidunt metus laoreet sed. Pellentesque at leo
+            venenatis, accumsan tortor vitae, pretium purus. Suspendisse
+            convallis urna non nunc blandit bibendum. Vivamus commodo gravida
+            sodales. Proin dictum nulla non diam faucibus tincidunt. Nam
+            pharetra ex non ante pharetra, quis gravida justo tempus. Sed at
+            augue eu enim feugiat vehicula. Fusce nec eros finibus, suscipit
+            ante ac, mollis nunc. Duis condimentum, neque sit amet fringilla
+            suscipit, velit mauris semper erat, eget elementum lacus massa et
+            erat. Etiam luctus malesuada sollicitudin.
+        </p>
+    </section>
+    <section>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at risus
+            vitae lorem vehicula pretium. Vivamus quis porttitor ex. Vivamus et
+            turpis venenatis, pellentesque libero eget, tristique nibh. Aenean
+            porta ipsum leo, nec eleifend metus semper vitae. Duis dapibus orci
+            purus, eget tincidunt metus laoreet sed. Pellentesque at leo
+            venenatis, accumsan tortor vitae, pretium purus. Suspendisse
+            convallis urna non nunc blandit bibendum. Vivamus commodo gravida
+            sodales. Proin dictum nulla non diam faucibus tincidunt. Nam
+            pharetra ex non ante pharetra, quis gravida justo tempus. Sed at
+            augue eu enim feugiat vehicula. Fusce nec eros finibus, suscipit
+            ante ac, mollis nunc. Duis condimentum, neque sit amet fringilla
+            suscipit, velit mauris semper erat, eget elementum lacus massa et
+            erat. Etiam luctus malesuada sollicitudin.
+        </p>
+    </section>
 </main>
+<footer id="right-panel-footer" class="panel">right panel footer</footer>
+
+<style>
+    :global(div#app),
+    :global(body) {
+        background-color: red;
+        height: 100vh;
+    }
+
+    :global(div#app) {
+        display: grid;
+        gap: 0.5rem;
+        padding: 0.5rem clamp(5rem, 8vw, 10rem);
+        grid-template-areas:
+            "leftPanel rightPanelHeader"
+            "leftPanel rightPanelBody"
+            "leftPanel rightPanelFooter";
+        grid-template-columns: 1fr 2fr;
+        grid-template-rows: auto 1fr auto;
+        max-width: 1440px;
+        margin: auto auto;
+    }
+
+    #left-panel {
+        grid-area: leftPanel;
+        background-color: green;
+    }
+
+    #right-panel-header {
+        grid-area: rightPanelHeader;
+        background-color: blue;
+    }
+
+    #right-panel-body {
+        grid-area: rightPanelBody;
+        background-color: yellow;
+        border: solid yellow;
+        border-width: 0.5rem;
+        overflow: auto;
+    }
+
+    #right-panel-footer {
+        grid-area: rightPanelFooter;
+        background-color: pink;
+    }
+
+    .panel {
+        border-radius: 0.5rem;
+        padding: 0.5rem;
+    }
+
+    section {
+        background-color: cyan;
+        border-radius: 0.5rem;
+        margin: 0.5rem auto;
+        padding: 0.5rem;
+    }
+</style>
