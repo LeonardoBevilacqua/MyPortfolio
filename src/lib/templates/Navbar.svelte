9bce4329f80861c6ed6c797e4512abfefbd8c60b
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2025-11-23 14:09:40 -0300

    adding nav bar content

--- src/lib/templates/Navbar.svelte
+++ src/lib/templates/Navbar.svelte
@@ -1,4 +1,33 @@
-<nav id="right-panel-header" class="rounded-lg p-2 bg-blue-300">right panel header</nav>
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+
+	type Routes = '/' | '/experiences' | '/portfolio';
+
+	function isActive(route: Routes): boolean {
+		if (route === '/') return page.url.pathname === route;
+		return page.url.pathname.replace('/', '').includes(route.replace('/', ''));
+	}
+</script>
+
+{#snippet link(route: Routes, label: string)}
+	<a
+		href={resolve(route)}
+		class={[
+			'text-2xl p-2 border border-solid border-transparent rounded-lg hover:text-dark-10 hover:border-dark-10',
+			isActive(route) && 'bg-dark-10 hover:text-white hover:border-white'
+		]}>{label}</a
+	>
+{/snippet}
+
+<nav
+	id="right-panel-header"
+	class="rounded-lg p-5 bg-dark-30 text-white flex gap-3 justify-center items-center flex-wrap"
+>
+	{@render link('/', 'Competências')}
+	{@render link('/experiences', 'Experiências')}
+	{@render link('/portfolio', 'Portfólio')}
+</nav>
 
 <style>
 	#right-panel-header {
