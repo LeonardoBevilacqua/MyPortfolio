cde2e8519ec768a515fbc3ce228f81bfaecd6516
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2025-11-29 15:12:24 -0300

    adding skills content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,63 @@
-{#snippet section()}
-	<section class="bg-cyan-300 rounded-lg my-2 mx-auto p-2">
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at risus vitae lorem vehicula
-			pretium. Vivamus quis porttitor ex. Vivamus et turpis venenatis, pellentesque libero eget,
-			tristique nibh. Aenean porta ipsum leo, nec eleifend metus semper vitae. Duis dapibus orci
-			purus, eget tincidunt metus laoreet sed. Pellentesque at leo venenatis, accumsan tortor vitae,
-			pretium purus. Suspendisse convallis urna non nunc blandit bibendum. Vivamus commodo gravida
-			sodales. Proin dictum nulla non diam faucibus tincidunt. Nam pharetra ex non ante pharetra,
-			quis gravida justo tempus. Sed at augue eu enim feugiat vehicula. Fusce nec eros finibus,
-			suscipit ante ac, mollis nunc. Duis condimentum, neque sit amet fringilla suscipit, velit
-			mauris semper erat, eget elementum lacus massa et erat. Etiam luctus malesuada sollicitudin.
-		</p>
-	</section>
+<script lang="ts">
+	import SectionHeader from '$lib/atoms/SectionHeader.svelte';
+	type Skill = { src: string; title: string };
+	type SkillType = { label: string; skills: Skill[] };
+
+	const skillTypes: SkillType[] = [
+		{
+			label: 'Desenvolvimento Backend',
+			skills: [
+				{ src: 'node-js', title: 'Node JS' },
+				{ src: 'spring-boot', title: 'Spring Framework' }
+			]
+		},
+		{
+			label: 'Desenvolvimento Frontend',
+			skills: [
+				{ src: 'angular', title: 'Angular' },
+				{ src: 'svelte', title: 'Svelte' },
+				{ src: 'tailwind-css', title: 'Tailwind CSS' },
+				{ src: 'bootstrap-logo', title: 'Bootstrap CSS' }
+			]
+		},
+		{
+			label: 'Banco de dados',
+			skills: [
+				{ src: 'postgres', title: 'PostgreSQL' },
+				{ src: 'mongodb', title: 'MongoDB' }
+			]
+		},
+		{
+			label: 'Cloud & Devops',
+			skills: [
+				{ src: 'aws', title: 'Amazon Web Services' },
+				{ src: 'docker', title: 'Docker' }
+			]
+		},
+		{
+			label: 'Desenvolvimento de jogos',
+			skills: [{ src: 'godot', title: 'Godot engine' }]
+		},
+		{
+			label: 'Design & Prototipagem',
+			skills: [{ src: 'figma', title: 'Figma' }]
+		}
+	];
+</script>
+
+{#snippet skillCard(src: string, title: string)}
+	<div class="bg-dark-60 inline-flex flex-col p-3 text-center rounded-3xl text-white grow">
+		<img src={`logos/${src}.svg`} alt={`${title} logo`} class="m-auto" />
+		<span class="mt-1 inline-block text-lg">{title}</span>
+	</div>
 {/snippet}
-<h1>right panel body</h1>
-{#each { length: 7 }}
-	{@render section()}
+
+<SectionHeader class="mb-4">Competências</SectionHeader>
+{#each skillTypes as skill (skill.label)}
+	<SectionHeader header="h3" class="mb-4">{skill.label}</SectionHeader>
+	<div class="grid @tablet:grid-cols-2 gap-4 mb-4">
+		{#each skill.skills as skillInfo (skillInfo.src)}
+			{@render skillCard(skillInfo.src, skillInfo.title)}
+		{/each}
+	</div>
 {/each}
