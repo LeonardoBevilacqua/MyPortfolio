41e88ac1b98d993470eefd3e8716059279c9637c
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2025-11-29 15:01:46 -0300

    updating main panel background

--- src/lib/templates/MainPanel.svelte
+++ src/lib/templates/MainPanel.svelte
@@ -4,7 +4,7 @@
 
 <main
 	style="grid-area: rightPanelBody;"
-	class="rounded-lg p-2 bg-yellow-300 border-[0.5rem] border-solid border-yellow-300 overflow-auto"
+	class="rounded-lg p-2 bg-dark-30 border-[0.5rem] border-solid border-dark-30 overflow-auto"
 >
 	{@render children()}
 </main>
