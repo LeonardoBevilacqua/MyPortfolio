f82d6ac30bfc66cb952dced208533d5d44743b38
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-05-01 11:49:29 -0300

    starting the base of portfolio view

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,1 +1,50 @@
-Portfólio
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import SectionHeader from '$lib/atoms/SectionHeader.svelte';
+	type Project = {
+		title: string;
+		subtitle: string;
+		descriptions: string[];
+		link: '/project';
+	};
+
+	const projects: Project[] = [
+		{
+			title: 'Project Title',
+			subtitle: 'Project subtitle',
+			descriptions: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent suscipit tortor eu ultricies dapibus. Vestibulum vestibulum ante eget ante malesuada mollis. Vivamus efficitur purus ut orci tristique, nec suscipit massa interdum. Aliquam a vehicula leo, non maximus est. Vivamus viverra ipsum nec massa fermentum euismod. Sed at purus nec sapien feugiat condimentum. Pellentesque maximus mauris vel tristique tempus. Nunc venenatis lectus eget nisl ornare euismod. Proin lectus ex, sagittis in dictum id, gravida eu lorem. Vestibulum vehicula vel ipsum congue mollis.',
+				'Maecenas accumsan ex elit, non volutpat libero ultricies ac. In rutrum, velit nec maximus vestibulum, massa nibh egestas odio, a maximus leo nulla nec velit. Proin et nibh hendrerit, venenatis leo at, sollicitudin ex. Sed lorem lorem, accumsan ac molestie posuere, laoreet sed sapien. Quisque dui risus, rhoncus eget felis eget, posuere dapibus justo. Vivamus et urna vitae sapien volutpat rutrum. In vel accumsan sem. Proin faucibus nulla sapien, dignissim consequat dolor vehicula vel. Suspendisse leo nisi, tristique sed ex non, interdum cursus dolor. Quisque vitae massa et nisi dignissim porta in molestie ligula. Integer cursus velit elit. Nullam a tellus lorem. Vestibulum pharetra sapien eget ultrices congue. Morbi in purus lacinia, ultricies eros non, aliquet quam. Donec malesuada efficitur aliquam. Aenean id ipsum non erat gravida vestibulum lobortis pharetra nulla.'
+			],
+			link: '/project'
+		},
+		{
+			title: 'Project Title 2',
+			subtitle: 'Project subtitle 2',
+			descriptions: [
+				'Quisque vestibulum arcu vitae lacus vulputate, sit amet imperdiet ex congue. Nam mollis erat eget tempus tincidunt. Donec blandit mi ligula, eget condimentum libero mattis ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam ultrices malesuada auctor. Quisque ac risus quis tortor efficitur volutpat. Quisque commodo lobortis rhoncus.',
+				'Nam gravida orci et nibh dignissim convallis. Proin pretium fermentum libero. Donec non leo vitae sapien scelerisque vehicula. Vestibulum faucibus enim vel quam consequat, et tincidunt dolor rhoncus. Pellentesque sed sapien sodales, luctus orci et, tempus lacus. Suspendisse eget lacus mi. Proin imperdiet justo id efficitur bibendum. ',
+				'In lacinia convallis turpis, vel feugiat felis finibus sit amet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc feugiat ut felis nec imperdiet. Ut placerat turpis porta magna mattis, non dictum justo faucibus. Curabitur non viverra mi. Quisque at sapien malesuada, consectetur mauris sit amet, tincidunt magna. Quisque congue dignissim metus, a auctor diam ullamcorper non. Nam hendrerit iaculis mauris, eu laoreet odio varius ac. Nulla fringilla odio quis ligula lobortis, in dapibus diam luctus. Ut bibendum, mauris eu bibendum malesuada, quam lacus efficitur massa, at porttitor nisi ante rhoncus sapien. Mauris aliquet finibus orci, facilisis tincidunt purus semper nec. Morbi sed imperdiet nulla. Nunc porttitor interdum nibh. Nam eu auctor mi, ac condimentum purus.'
+			],
+			link: '/project'
+		}
+	];
+</script>
+
+{#snippet projectCard({ title, subtitle, descriptions, link }: Project)}
+	<section class="bg-dark-60 text-white rounded-lg p-2 gap-2 flex flex-col">
+		<h3 class="text-dark-10 text-lg font-bold">{title}</h3>
+		<h4 class="font-medium mb-2">{subtitle}</h4>
+		{#each descriptions as description, index (index)}
+			<p>{description}</p>
+		{/each}
+		<a href={resolve(link)} class="bg-dark-30 rounded-lg p-3 inline-block m-auto">Ver mais sobre</a>
+	</section>
+{/snippet}
+
+<SectionHeader class="mb-4">Portfólio</SectionHeader>
+<article class="flex flex-col gap-4">
+	{#each projects as project, index (index)}
+		{@render projectCard(project)}
+	{/each}
+</article>
