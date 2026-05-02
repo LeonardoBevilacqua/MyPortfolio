a94aca0d6cdc7deb2229cd64f94772e0abcfd987
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-05-02 13:07:30 -0300

    moving project route

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -5,7 +5,7 @@
 		title: string;
 		subtitle: string;
 		descriptions: string[];
-		link: '/project';
+		link: `/portfolio/${string}`;
 		imgLink: string;
 	};
 
@@ -17,7 +17,7 @@
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent suscipit tortor eu ultricies dapibus. Vestibulum vestibulum ante eget ante malesuada mollis. Vivamus efficitur purus ut orci tristique, nec suscipit massa interdum. Aliquam a vehicula leo, non maximus est. Vivamus viverra ipsum nec massa fermentum euismod. Sed at purus nec sapien feugiat condimentum. Pellentesque maximus mauris vel tristique tempus. Nunc venenatis lectus eget nisl ornare euismod. Proin lectus ex, sagittis in dictum id, gravida eu lorem. Vestibulum vehicula vel ipsum congue mollis.',
 				'Maecenas accumsan ex elit, non volutpat libero ultricies ac. In rutrum, velit nec maximus vestibulum, massa nibh egestas odio, a maximus leo nulla nec velit. Proin et nibh hendrerit, venenatis leo at, sollicitudin ex. Sed lorem lorem, accumsan ac molestie posuere, laoreet sed sapien. Quisque dui risus, rhoncus eget felis eget, posuere dapibus justo. Vivamus et urna vitae sapien volutpat rutrum. In vel accumsan sem. Proin faucibus nulla sapien, dignissim consequat dolor vehicula vel. Suspendisse leo nisi, tristique sed ex non, interdum cursus dolor. Quisque vitae massa et nisi dignissim porta in molestie ligula. Integer cursus velit elit. Nullam a tellus lorem. Vestibulum pharetra sapien eget ultrices congue. Morbi in purus lacinia, ultricies eros non, aliquet quam. Donec malesuada efficitur aliquam. Aenean id ipsum non erat gravida vestibulum lobortis pharetra nulla.'
 			],
-			link: '/project',
+			link: '/portfolio/project-title-1',
 			imgLink: '/images/portfolio/choose-your-game/choose_your_game_01.PNG'
 		},
 		{
@@ -28,7 +28,7 @@
 				'Nam gravida orci et nibh dignissim convallis. Proin pretium fermentum libero. Donec non leo vitae sapien scelerisque vehicula. Vestibulum faucibus enim vel quam consequat, et tincidunt dolor rhoncus. Pellentesque sed sapien sodales, luctus orci et, tempus lacus. Suspendisse eget lacus mi. Proin imperdiet justo id efficitur bibendum. ',
 				'In lacinia convallis turpis, vel feugiat felis finibus sit amet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc feugiat ut felis nec imperdiet. Ut placerat turpis porta magna mattis, non dictum justo faucibus. Curabitur non viverra mi. Quisque at sapien malesuada, consectetur mauris sit amet, tincidunt magna. Quisque congue dignissim metus, a auctor diam ullamcorper non. Nam hendrerit iaculis mauris, eu laoreet odio varius ac. Nulla fringilla odio quis ligula lobortis, in dapibus diam luctus. Ut bibendum, mauris eu bibendum malesuada, quam lacus efficitur massa, at porttitor nisi ante rhoncus sapien. Mauris aliquet finibus orci, facilisis tincidunt purus semper nec. Morbi sed imperdiet nulla. Nunc porttitor interdum nibh. Nam eu auctor mi, ac condimentum purus.'
 			],
-			link: '/project',
+			link: '/portfolio/project-title-2',
 			imgLink: '/images/portfolio/choose-your-game/choose_your_game_02.PNG'
 		}
 	];
