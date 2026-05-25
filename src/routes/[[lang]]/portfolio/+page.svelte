bedaca69f93f41727d05166b9a4d643fd27efcfb
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-05-25 20:25:52 -0300

    fixing project cards issue

--- src/routes/[[lang]]/portfolio/+page.svelte
+++ src/routes/[[lang]]/portfolio/+page.svelte
@@ -19,31 +19,25 @@
 )}
 	{@const { link, imgLink } = resource}
 	<section
-		class="bg-dark-60 text-white rounded-lg p-2 gap-2 grid @min-[720px]:grid-cols-2 @desktop:grid-cols-1"
+		class="bg-dark-60 text-white rounded-lg p-2 gap-2 grid @min-[720px]:grid-cols-2 @min-[720px]:grid-rows-[auto_1fr] @min-desktop:grid-cols-1 @min-desktop:grid-rows-[repeat(3,auto)]"
 	>
-		<div
-			class="content-center @max-[720px]:hidden @desktop:col-span-3 @desktop:row-start-2 {reverse &&
-				'col-start-2'} @min-[720px]:row-span-3 @desktop:row-span-1"
-		>
-			<img src={imgLink} class="max-w-full h-auto rounded-lg" alt={title} />
-		</div>
-
-		<div class=" @desktop:col-span-3 row-start-1">
+		<div class="row-auto {reverse && '@min-[720px]:col-start-2 @min-desktop:col-start-1'}">
 			<h3 class="text-dark-10 text-lg font-bold">{title}</h3>
 			<h4 class="font-medium mb-2">{subtitle}</h4>
 		</div>
-
 		<div
-			class="{reverse
-				? '@min-[720px]:col-start-1'
-				: '@min-[720px]:col-start-2'} @desktop:col-span-3 @min-[720px]:row-span-2 flex flex-col"
+			class="hidden @min-[720px]:block @min-[720px]:row-span-2 @min-desktop:row-span-1 {reverse &&
+				'@min-[720px]:col-start-1 @min-[720px]:row-start-1 @min-desktop:row-start-2'}"
 		>
+			<img src={imgLink} class="max-w-full h-auto rounded-lg" alt={title} />
+		</div>
+		<div class="flex flex-col">
 			{#each descriptions as description, index (index)}
 				<p>{description}</p>
 			{/each}
 			<a
 				href={resolve(langRoute + link)}
-				class="bg-dark-30 hover:bg-dark-10 transition-colors duration-300 rounded-lg p-3 inline-block m-auto @desktop:mt-2 @max-[720px]:mt-2"
+				class="bg-dark-30 hover:bg-dark-10 transition-colors duration-300 rounded-lg p-3 inline-block m-auto @min-[720px]:mb-0 @min-desktop:mt-2 @max-[720px]:mt-2"
 				>{seeMore}</a
 			>
 		</div>
